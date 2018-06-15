020ebc87a7cb121a9b4078adceddf3ee3268da6c
Matthew R <39703499+matthewrice13@users.noreply.github.com>
2018-06-15 18:11:40 +0100

    Commit 2 - 17/06/2018@18:10

--- CraftMaps/settings.py
+++ CraftMaps/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'CraftMaps.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 #######################################################################
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CraftMapsDB',
+        'USER': 'CraftMaps',
+        'PASSWORD': 'CraftM3355',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
