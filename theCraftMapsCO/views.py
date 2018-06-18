3bd3058eff702526fef30b47b9e6af57b0fd29e0
Matthew R <39703499+MatthewRice13@users.noreply.github.com>
2018-06-18 17:06:45 +0100

    Commit 9.1 - 18/06/2018@17:05
    - Front End Testing files
    -- get name

--- theCraftMapsCO/views.py
+++ theCraftMapsCO/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Brewery_Table
 # Create your views here.
 def home(request):
-    brewery = Brewery_Table.objects.get(id=2)
+    brewery = Brewery_Table.objects.get(id=1)
     context = {
         'brewery': brewery
     }
