f3fdf6bcc9faf94a519739be8d4f921e82381a6b
Matthew R <39703499+MatthewRice13@users.noreply.github.com>
2018-06-18 13:56:50 +0100

    Commit 6 - 18/06/2018@14:00
    - updated mysql models

--- theCraftMapsCO/models.py
+++ theCraftMapsCO/models.py
@@ -10,10 +10,22 @@ class Brewery_Table(models.Models):
     Brewery_Longitude = models.DecimalField(max_digits=10, decimal_places=10)
     Brewery_URL = models.URLField(max_length=200, null=True, blank=True)
 
-class BeerTable(models.Models):
+
+class Beer_Table(models.Models):
     Beer_ID = models.IntegerField(max_length=2000)
     Beer_Name = models.CharField(max_length=255)
     Beer_Brewery = models.CharField(max_length=255)
     Beer_Type = models.CharField(max_length=150)
     Beer_Percent = models.DecimalField(max_digits=2, decimal_places=2)
     Beer_Rating = models.DecimalField(max_digits=2, decimal_places=2)
+
+
+class Brewery_Types(models.Models):
+    ID = models.IntegerField(max_length=10000)
+    Brewery_ID = models.IntegerField(max_length=2000)
+    Type_ID = models.IntegerField(max_length=2000)
+
+
+class Types_Table(models.Models):
+    Type_ID = models.IntegerField(max_length=10000)
+    Type_Name = models.IntegerField(max_length=2000)
