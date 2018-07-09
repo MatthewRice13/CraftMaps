7fa569f9fbe35fd18fa9596efb21463624b4d38a
Matthew R <39703499+MatthewRice13@users.noreply.github.com>
2018-07-09 15:26:02 +0100

    Commit: 09/07/2018@15:30
    - models

--- theCraftMapsCO/models.py
+++ theCraftMapsCO/models.py
@@ -4,12 +4,12 @@ from django.db import models
 # Create your models here.
 class Brewery_Table(models.Model):
     Brewery_Name = models.CharField(max_length=90)
-    Brewery_Region = modesl.CharField(max_length=90)
-    Brewery_Address = models.CharField(max_length=500)
+    Brewery_Region = models.CharField(max_length=90, default='Dublin')
+    Brewery_Address = models.CharField(max_length=500, default='Dublin, Ireland')
     Brewery_Type = models.CharField(max_length=50, default='Brewery')
-    Brewer_Rating = models.DecimalField(max_digits=2, decimal_palces=2, default=2.5)
-    Brewery_Latitude = models.DecimalField(max_digits=10, decimal_places=10)
-    Brewery_Longitude = models.DecimalField(max_digits=10, decimal_places=10)
+    Brewer_Rating = models.DecimalField(max_digits=2, decimal_places=2, default=2.5)
+    Brewery_Latitude = models.DecimalField(max_digits=10, decimal_places=8)
+    Brewery_Longitude = models.DecimalField(max_digits=10, decimal_places=8)
     Brewery_URL = models.URLField(max_length=255, null=True, blank=True)
     Brewery_Twitter = models.URLField(max_length=255, null=True, blank=True)
     Brewery_Facebook = models.URLField(max_length=255, null=True, blank=True)
