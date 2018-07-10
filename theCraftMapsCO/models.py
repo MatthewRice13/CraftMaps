3e6da0304885ad8823b15716c4ec8adf3c3ff762
Matthew R <39703499+MatthewRice13@users.noreply.github.com>
2018-07-10 13:19:59 +0100

    Commit: 10/07/2018@15:30
    - models

--- theCraftMapsCO/models.py
+++ theCraftMapsCO/models.py
@@ -7,7 +7,7 @@ class Brewery_Table(models.Model):
     Brewery_Region = models.CharField(max_length=90, default='Dublin')
     Brewery_Address = models.CharField(max_length=500, default='Dublin, Ireland')
     Brewery_Type = models.CharField(max_length=50, default='Brewery')
-    Brewer_Rating = models.DecimalField(max_digits=3, decimal_places=2, default=2.5)
+    Brewer_Rating = models.DecimalField(max_digits=4, decimal_places=2)
     Brewery_Latitude = models.DecimalField(max_digits=10, decimal_places=8)
     Brewery_Longitude = models.DecimalField(max_digits=10, decimal_places=8)
     Brewery_URL = models.URLField(max_length=255, null=True, blank=True)
