e69563376b68f362c1cca87d3109c6c91b9f85d9
Matthew R <39703499+MatthewRice13@users.noreply.github.com>
2018-07-14 10:46:31 +0100

    Commit: 14/07/2018@10:45
    - models

--- theCraftMapsCO/models.py
+++ theCraftMapsCO/models.py
@@ -7,7 +7,7 @@ class Brewery_Table(models.Model):
     Brewery_Region = models.CharField(max_length=90, default='Dublin')
     Brewery_Address = models.CharField(max_length=500, default='Dublin, Ireland')
     Brewery_Type = models.CharField(max_length=50, default='Brewery')
-    Brewer_Rating = models.DecimalField(max_digits=4, decimal_places=2)
+    Brewer_Rating = models.DecimalField(max_digits=5, decimal_places=3)
     Brewery_Latitude = models.DecimalField(max_digits=13, decimal_places=10)
     Brewery_Longitude = models.DecimalField(max_digits=13, decimal_places=10)
     Brewery_URL = models.URLField(max_length=255, null=True, blank=True)
@@ -27,3 +27,26 @@ class Beer_Table(models.Model):
 
     def _str_(self):
         return self.Beer_Name
+
+
+class User_Table(models.Model):
+    User_Id = models.IntegerField()
+    User_Favorite_Brewery_Type = models.CharField(max_length=90)
+    User_Max_Distance = models.IntegerField(max_digits=10, default=20)
+    User_Beer_Stout = models.BooleanField(default=false)
+    User_Beer_Lager = models.BooleanField(default=false)
+    User_Beer_IPA = models.BooleanField(default=false)
+    User_Beer_Cider = models.BooleanField(default=false)
+    User_Beer_Pilsner = models.BooleanField(default=false)
+    User_Beer_Ale = models.BooleanField(default=false)
+    User_Beer_Weiss = models.BooleanField(default=false)
+
+
+class User_Brewery_Ratings(models.Model):
+    Brewery_Id = models.IntegerField()
+    User_Id = models.IntegerField()
+    User_Brewery_Rating = models.DecimalField(max_length=4, decimal_places=2)
+    Rating_Time_Stamp = models.DateField(max_length=155)
+    User_Do_Tour = models.BooleanField(default=False)
+    User_Drink_Beer = models.BooleanField(default=False)
+    User_Buy_Merch = models.BooleanField(default=False)
